696e98d04fe528747d69d038a174b82e214285b4
An <abd21@ic.ac.uk>
2025-10-07 10:19:16 +0100

    Assignment 3 - tiff2png.sh

--- week1/code/tiff2png.sh
+++ week1/code/tiff2png.sh
@@ -5,8 +5,27 @@
 #
 # Date: October 2025
 
-for f in *.tif;
-    do
-        echo "Converting $f";
-        convert "$f" "$(basename "f" .tif).png";
-    done
+# ORIGINAL CODE
+#for f in *.tif;
+#    do
+#        echo "Converting $f";
+#        convert "$f" "$(basename "f" .tif).png";
+#    done
+
+echo "Please enter the .tif file to convert to .png:"  
+read f
+
+if [[ "$f" != *.tif ]]; then
+    echo "Input file must have a .tif extension."
+    exit 1
+fi
+
+file_path="week1/data/$f"
+
+if [[ ! -f "$file_path" ]]; then
+    echo "No file can be found under that input."
+    exit 1
+fi
+
+echo "Converting $f"; 
+convert "$file_path" "week1/data/$(basename "$f" .tif).png"
